c0d7e1b71fa853026a7d8f3457f68ac5701ef82d
misabiko <misabiko@gmail.com>
2020-12-27 15:54:43 -0500

    Managed a request to proxy :O

--- src/soshalthing.nim
+++ src/soshalthing.nim
@@ -1,7 +1,8 @@
 include karax / prelude
-import dom, timeline
+import dom, timeline, twitter/service
 
 let t = Timeline(name: "Home", articles: @["0", "1", "2"])
+discard getHomeTimeline()
 
 proc createDom(): VNode =
     result = buildHtml(tdiv):
@@ -13,3 +14,4 @@ setRenderer createDom
 # TODO Request home timeline
 # TODO Render tweet VNode
 # TODO Prune preludes
+# TODO Obey the nimble gods
--- src/twitter/service.nim
+++ src/twitter/service.nim
@@ -1,1 +1,26 @@
+import fetch, asyncjs, json, sequtils, options, tweet
 
+type
+    TimelinePayload = object
+        posts: seq[Post]
+        reposts: seq[Repost]
+        quotes: seq[Quote]
+        newArticles: seq[string]
+
+proc parseTweets(tweets: JsonNode): TimelinePayload =
+    for t in tweets:
+        let parsed = parseTweet(t)
+
+        if not result.posts.any(proc (p: Post): bool = p.id == parsed.post.id):
+            result.posts.add(parsed.post)
+        
+        if parsed.repost.isSome:
+            result.reposts.add(parsed.repost.get())
+        elif parsed.quote.isSome:
+            result.quotes.add(parsed.quote.get())
+        #else:
+
+proc getHomeTimeline*() {.async.} =
+    let tweets = await fetch("http://127.0.0.1:5000/home_timeline").toJsonNode()
+    let payload = parseTweets(tweets)
+    echo payload.posts[0].text
