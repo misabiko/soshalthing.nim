0d7b56f02923b133bb740f3e1a6a76408cd9e0c1
misabiko <misabiko@gmail.com>
2021-02-28 16:54:29 -0500

    Added expand button

--- src/soshalthingpkg/sidebar.nim
+++ src/soshalthingpkg/sidebar.nim
@@ -1,5 +1,13 @@
-import karax / [vdom, karaxdsl]
+import karax / [karax, vdom, karaxdsl]
+
+var expanded = false
 
 proc sidebar*(): VNode =
     buildHtml(nav(id = "sidebar")):
-        tdiv(id = "sidebarButtons")
+        tdiv(id = "sidebarButtons"):
+            button(class="refreshTimeline"):
+                span(class="icon"):
+                    italic(class="fas fa-2x fa-angle-double-right")
+
+                proc onclick() =
+                    echo "EXPAND"
